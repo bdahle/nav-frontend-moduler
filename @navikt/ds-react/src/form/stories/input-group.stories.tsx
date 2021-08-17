a123b707cc9e12da03d882bd4b495516a77b0701
Ken <ken.aleksander@gmail.com>
2021-08-17 11:07:33 +0200

    oppdatert story

--- @navikt/ds-react/src/form/stories/input-group.stories.tsx
+++ @navikt/ds-react/src/form/stories/input-group.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Fieldset, InputGroup } from "../index";
 import { Meta } from "@storybook/react/types-6-0";
-import { Button, TextField } from "../../index";
+import { Button, Select, TextField } from "../../index";
 import { Search } from "@navikt/ds-icons";
 export default {
   title: "ds-react/form/input-group",
@@ -20,8 +20,8 @@ export const All = () => {
         <div>
           <Search />
         </div>
-        <Button variant="action">Søk</Button>
         <TextField label="Textfield label" />
+        <Button variant="action">Søk</Button>
       </InputGroup>
 
       <h2>InputGroup small</h2>
@@ -34,12 +34,27 @@ export const All = () => {
         <div>
           <Search />
         </div>
+        <TextField size="s" hideLabel label="Textfield label" />
         <Button size="s" variant="action">
           Søk
         </Button>
-        <TextField size="s" hideLabel label="Textfield label" />
       </InputGroup>
 
+      <h2>InputGroup m select</h2>
+
+      <InputGroup
+        label="Søk for sider om foreldrepenger"
+        description="Skriv in stikkord om foreldrepenger"
+        error="Errormsg"
+      >
+        <div>
+          <Search />
+        </div>
+        <Select hideLabel label="Textfield label">
+          <option>Valg 1</option>
+        </Select>
+        <Button variant="action">Søk</Button>
+      </InputGroup>
       <h2>InputGroup m error</h2>
 
       <InputGroup
